65a8e54d1e1697a4520f8e5a84fe8d8a01ad4ec8
Alisson Nunes <alisson@alisson-SVD11215CDB>
2016-08-21 17:38:24 -0300

    adicionando o section e o footer

--- src/todoquiescent/core.cljs
+++ src/todoquiescent/core.cljs
@@ -2,14 +2,28 @@
   (:require [quiescent.core :as q]
             [quiescent.dom :as d]))
 
+(q/defcomponent Footer
+  "Footer"
+  []
+  (d/h3 {}
+        "Este é o footer"))
+
 (q/defcomponent TodoApp
    "Entire app"
    [model]
    (d/div {}
-          (d/header {}
+          (d/header {:className "header"}
                     (d/h1 {} 
                           "todos")
-                    (d/input {:className "new-todo"}))))
+                    (d/input {:className "new-todo"}))
+          (when (pos? (count (:todos model)))
+            (d/section {:className "main"}
+                       (d/input {:className "toggle-all"})
+                       (d/label {:htmlFor "toggle-all"}
+                                "Mark all as complete")
+                       (d/ul {:className "todo-list"})))
+          (when (pos? (count (:todos model)))
+            (Footer))))
 
 (def my-model {:todos [{:id 1357 :title "fazer aplicativo de ensino de línguas" :completed true}
                        {:id 6204 :title "vender muito" :completed false}]
