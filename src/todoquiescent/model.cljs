0ead9dfe5673c71b93e81e950d11d44085caa886
Alisson Nunes <alisson@alisson-SVD11215CDB>
2016-09-01 19:30:18 -0300

    estado inicial vazio

--- src/todoquiescent/model.cljs
+++ src/todoquiescent/model.cljs
@@ -1,9 +1,7 @@
 (ns todoquiescent.model
   (:require [clojure.core.async :as async]))
 
-(def model-todo (atom {:todos [#_{:id 1357 :title "fazer aplicativo de ensino de espanhol" :completed true}                                    
-                               {:id 6204 :title "marketing digital" :completed false}
-                               #_{:id 4369 :title "ensinar React.js" :completed true}]
+(def model-todo (atom {:todos []
                        :view-mode :all}))
 
 (def update-model-channel (async/chan))
