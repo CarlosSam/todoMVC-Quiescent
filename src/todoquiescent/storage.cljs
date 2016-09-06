4e7d33bf2f9f9cb8905a16f375481cc24b05da94
Alisson Nunes <alisson@alisson-SVD11215CDB>
2016-09-06 15:28:55 -0300

    simplificando o código de armazenamento

--- src/todoquiescent/storage.cljs
+++ src/todoquiescent/storage.cljs
@@ -1,30 +1,16 @@
 (ns todoquiescent.storage
-  (:require [todoquiescent.model :refer [model-todo]]))
+  (:require [todoquiescent.model :refer [model-todo]]
+            [cljs.reader :as reader]))
 
 (def LOCAL_STORAGE_NAMESPACE "todos-quiescent-cljs")
 
 (defn load-todos []
-  (->> LOCAL_STORAGE_NAMESPACE 
-       (.getItem js/localStorage) 
-       (.parse js/JSON) 
-       js->clj 
-       ((fn [mp]
-          (reduce (fn [s k]
-                    (let [v (get mp k)
-                          v (if (string? v)
-                              (keyword v)
-                              (vec (map #(reduce (fn [sv kv]
-                                                   (assoc sv (keyword kv) (get % kv)))
-                                                 {}
-                                                 (keys %))
-                                        v)))]
-                      (assoc s (keyword k) v)))
-                  {}
-                  (keys mp))))
+  (->> LOCAL_STORAGE_NAMESPACE
+       (.getItem js/localStorage)
+       reader/read-string
        (reset! model-todo)))
 
 (defn store-todos []
   (->> @model-todo
-       clj->js
-       (.stringify js/JSON)
+       str
        (.setItem js/localStorage LOCAL_STORAGE_NAMESPACE)))
